181cee055c805815cf6d921141528c776016dcec
Alexnoff <alexnoff73@gmail.com>
2020-09-24 14:31:27 +0200

    test "if the text is longer than the defined lenght, should return the complete text"

--- src/services/split/split.test.js
+++ src/services/split/split.test.js
@@ -6,4 +6,8 @@ describe('services/split/split', () => {
     expect(split('4963813675', 2)).toEqual(['49', '63', '81', '36', '75']);
     expect(split('792463079965448031', 4)).toEqual(['79', '2463', '0799', '6544', '8031']);
   });
+  it('if the text is longer than the defined lenght, should return the complete text', () => {
+    expect(split('1587', 5)).toEqual(['1587']);
+    expect(split('57639561', 10)).toEqual(['57639561']);
+  });
 });
