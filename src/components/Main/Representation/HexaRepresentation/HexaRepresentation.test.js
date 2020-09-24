524b152541c544fd112f8a209fce522e02328be8
Arelle <clerc.angelique@gmail.com>
2020-09-24 16:28:56 +0200

    [Update] test HexaRepresentation.jsx 100%

--- src/components/Main/Representation/HexaRepresentation/HexaRepresentation.test.js
+++ src/components/Main/Representation/HexaRepresentation/HexaRepresentation.test.js
@@ -1,10 +1,15 @@
 import { shallow } from 'enzyme';
 import React from 'react';
+import ColorNumber from '../ColorNumber/ColorNumber';
 import HexaRepresentation from './HexaRepresentation';
 
 describe('HexaRepresentation', () => {
   it('shouldnt display anything if no number given', () => {
     const component = shallow(<HexaRepresentation />);
     expect(component.isEmptyRender()).toEqual(true);
+  });
+  it('should return 2 if number = 2', () => {
+    const component = shallow(<HexaRepresentation number={2} />);
+    expect(component.contains(<ColorNumber numbers={['2']} />)).toEqual(true);
   });
 });
