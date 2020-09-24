ed33ff641f2902ea741a62c7834eef7b120e5291
Bastien <ba2411@hotmail.fr>
2020-09-24 14:05:47 +0200

    [add] test should return IV if number = 4

--- src/services/toRoman/toRoman.test.js
+++ src/services/toRoman/toRoman.test.js
@@ -5,4 +5,7 @@ describe('toRoman', () => {
   it('should return I if number = 1', () => {
     expect(toRoman(1)).toEqual('I');
   });
+  it('should return IV if number = 4', () => {
+    expect(toRoman(4)).toEqual('IV');
+  });
 });
